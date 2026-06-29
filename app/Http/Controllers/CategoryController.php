a30bef833f294931ef18e6723ed54c1329dc0ad3
Linatou <linatoudao7@gmail.com>
2026-06-29 11:14:02 +0000

    Ajout de l'API de creation de categorie

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,9 +8,25 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     public function index()
+    
     {
         $categories = Category::all();
 
         return response()->json($categories);
     }
+
+    public function store(Request $request)
+{
+    $request->validate([
+        'name' => 'required|max:255',
+        'description' => 'nullable'
+    ]);
+
+    $category = Category::create([
+        'name' => $request->name,
+        'description' => $request->description
+    ]);
+
+    return response()->json($category, 201);
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 
 Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/categories', [CategoryController::class, 'store']);
